054bb2b36bf835fdbd7784054c35bb6b37f798d8
Daniel Schneider <daschne8@gmail.com>
2019-06-20 17:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function sayHey(){
 function sayHeyFriend(name){
   return `Hey, ${name}!`
 }
+function favoriteIceCream(flavor){
+  return `I love ${flavor}`;
+}
+function shouting(phrase){
+  return phrase.toUpperCase();
+}
+function roundDown(number){
+  return Math.floor(number)
+}
